186a3b83e4df6b885083ae63067d908894b16cd0
Arsu9869 <30731423+Arsu9869@users.noreply.github.com>
2024-10-25 21:00:17 +0530

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-git init
 
+git config --user.email "ramesharavind900@gmail.com
+git config --user.name  "ARSU9869"
+mkdir myfirstrepo
+git init
 git clone https://github.com/Arsu9869/October.git
+
+
